0ff1467fe4af6e6de03bf3465e231342368b71c7
Pit Garbe <leberwurstsaft@me.com>
2015-01-12 12:09:57 +0100

    Code style

--- PatternLock/TestViewController.m
+++ PatternLock/TestViewController.m
@@ -10,16 +10,15 @@
 #import "NormalCircle.h"
 
 
-@interface TestViewController ()<LockScreenDelegate>
+@interface TestViewController () <LockScreenDelegate>
 
 @property (nonatomic) NSInteger wrongGuessCount;
 @end
 
 @implementation TestViewController
-@synthesize infoLabelStatus,wrongGuessCount;
+@synthesize infoLabelStatus, wrongGuessCount;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -27,122 +26,120 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-	self.view.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
+    self.view.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
 }
 
 
-- (void)viewDidAppear:(BOOL)animated
-{	
-	[super viewDidAppear:animated];
-	
-	self.lockScreenView = [[SPLockScreen alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width)];
-	self.lockScreenView.center = self.view.center;
-	self.lockScreenView.delegate = self;
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
 
-	[self.view addSubview:self.lockScreenView];
-	
-	self.infoLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 20)];
-	self.infoLabel.backgroundColor = [UIColor clearColor];
-	self.infoLabel.font = [UIFont systemFontOfSize:16];
-	self.infoLabel.textColor = [UIColor whiteColor];
-	self.infoLabel.textAlignment = NSTextAlignmentCenter;
-	[self.view addSubview:self.infoLabel];
-	
-	[self updateOutlook];
-	 
-	
-	// Test with Circular Progress
+    self.lockScreenView          = [[SPLockScreen alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.width)];
+    self.lockScreenView.center   = self.view.center;
+    self.lockScreenView.delegate = self;
+
+    [self.view addSubview:self.lockScreenView];
+
+    self.infoLabel                 = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 20)];
+    self.infoLabel.backgroundColor = [UIColor clearColor];
+    self.infoLabel.font            = [UIFont systemFontOfSize:16];
+    self.infoLabel.textColor       = [UIColor whiteColor];
+    self.infoLabel.textAlignment   = NSTextAlignmentCenter;
+    [self.view addSubview:self.infoLabel];
+
+    [self updateOutlook];
+
+
+    // Test with Circular Progress
 }
 
 
-- (void)updateOutlook
-{
-	switch (self.infoLabelStatus) {
-		case InfoStatusFirstTimeSetting:
-			self.infoLabel.text = @"Set a Pattern !";
-			break;
-		case InfoStatusConfirmSetting:
-			self.infoLabel.text = @"Confirm the Pattern !";
-			break;
-		case InfoStatusFailedConfirm:
-			self.infoLabel.text = @"Failed to confirm, please retry";
-			break;
-		case InfoStatusNormal:
-			self.infoLabel.text = @"Draw previously set pattern to go in";
-			break;
-		case InfoStatusFailedMatch:
-			self.infoLabel.text = [NSString stringWithFormat:@"Wrong Guess # %d, try again !",self.wrongGuessCount];
-			break;
-		case InfoStatusSuccessMatch:
-			self.infoLabel.text = @"Welcome !";
-			break;
-			
-		default:
-			break;
-	}
-	
+- (void)updateOutlook {
+    switch (self.infoLabelStatus) {
+        case InfoStatusFirstTimeSetting:
+            self.infoLabel.text = @"Set a Pattern !";
+            break;
+        case InfoStatusConfirmSetting:
+            self.infoLabel.text = @"Confirm the Pattern !";
+            break;
+        case InfoStatusFailedConfirm:
+            self.infoLabel.text = @"Failed to confirm, please retry";
+            break;
+        case InfoStatusNormal:
+            self.infoLabel.text = @"Draw previously set pattern to go in";
+            break;
+        case InfoStatusFailedMatch:
+            self.infoLabel.text = [NSString stringWithFormat:@"Wrong Guess # %d, try again !", self.wrongGuessCount];
+            break;
+        case InfoStatusSuccessMatch:
+            self.infoLabel.text = @"Welcome !";
+            break;
+
+        default:
+            break;
+    }
+
 }
 
 
 #pragma -LockScreenDelegate
 
-- (void)lockScreen:(SPLockScreen *)lockScreen didEndWithPattern:(NSNumber *)patternNumber
-{
-	NSUserDefaults *stdDefault = [NSUserDefaults standardUserDefaults];
-	NSLog(@"self status: %d",self.infoLabelStatus);
-	switch (self.infoLabelStatus) {
-		case InfoStatusFirstTimeSetting:
-			[stdDefault setValue:patternNumber forKey:kCurrentPatternTemp];
-			self.infoLabelStatus = InfoStatusConfirmSetting;
-			[self updateOutlook];
-			break;
-		case InfoStatusFailedConfirm:
-			[stdDefault setValue:patternNumber forKey:kCurrentPatternTemp];
-			self.infoLabelStatus = InfoStatusConfirmSetting;
-			[self updateOutlook];
-			break;
-		case InfoStatusConfirmSetting:
-			if([patternNumber isEqualToNumber:[stdDefault valueForKey:kCurrentPatternTemp]]) {
-				[stdDefault setValue:patternNumber forKey:kCurrentPattern];
-				[self dismissViewControllerAnimated:YES completion:nil];
-			}
-			else {
-				self.infoLabelStatus = InfoStatusFailedConfirm;
-				[self updateOutlook];
-			}
-			break;
-		case  InfoStatusNormal:
-			if([patternNumber isEqualToNumber:[stdDefault valueForKey:kCurrentPattern]]) [self dismissViewControllerAnimated:YES completion:nil];
-			else {
-				self.infoLabelStatus = InfoStatusFailedMatch;
-				self.wrongGuessCount ++;
-				[self updateOutlook];
-			}
-			break;
-		case InfoStatusFailedMatch:
-			if([patternNumber isEqualToNumber:[stdDefault valueForKey:kCurrentPattern]]) [self dismissViewControllerAnimated:YES completion:nil];
-			else {
-				self.wrongGuessCount ++;
-				self.infoLabelStatus = InfoStatusFailedMatch;
-				[self updateOutlook];
-			}
-			break;
-		case InfoStatusSuccessMatch:
-			[self dismissViewControllerAnimated:YES completion:nil];
-			break;
-			
-		default:
-			break;
-	}
+- (void)lockScreen:(SPLockScreen *)lockScreen didEndWithPattern:(NSNumber *)patternNumber {
+    NSUserDefaults *stdDefault = [NSUserDefaults standardUserDefaults];
+    NSLog(@"self status: %d", self.infoLabelStatus);
+    switch (self.infoLabelStatus) {
+        case InfoStatusFirstTimeSetting:
+            [stdDefault setValue:patternNumber forKey:kCurrentPatternTemp];
+            self.infoLabelStatus = InfoStatusConfirmSetting;
+            [self updateOutlook];
+            break;
+        case InfoStatusFailedConfirm:
+            [stdDefault setValue:patternNumber forKey:kCurrentPatternTemp];
+            self.infoLabelStatus = InfoStatusConfirmSetting;
+            [self updateOutlook];
+            break;
+        case InfoStatusConfirmSetting:
+            if ([patternNumber isEqualToNumber:[stdDefault valueForKey:kCurrentPatternTemp]]) {
+                [stdDefault setValue:patternNumber forKey:kCurrentPattern];
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }
+            else {
+                self.infoLabelStatus = InfoStatusFailedConfirm;
+                [self updateOutlook];
+            }
+            break;
+        case  InfoStatusNormal:
+            if ([patternNumber isEqualToNumber:[stdDefault valueForKey:kCurrentPattern]])
+                [self dismissViewControllerAnimated:YES completion:nil];
+            else {
+                self.infoLabelStatus = InfoStatusFailedMatch;
+                self.wrongGuessCount++;
+                [self updateOutlook];
+            }
+            break;
+        case InfoStatusFailedMatch:
+            if ([patternNumber isEqualToNumber:[stdDefault valueForKey:kCurrentPattern]])
+                [self dismissViewControllerAnimated:YES completion:nil];
+            else {
+                self.wrongGuessCount++;
+                self.infoLabelStatus = InfoStatusFailedMatch;
+                [self updateOutlook];
+            }
+            break;
+        case InfoStatusSuccessMatch:
+            [self dismissViewControllerAnimated:YES completion:nil];
+            break;
+
+        default:
+            break;
+    }
 }
 
 - (void)viewDidUnload {
-	[self setInfoLabel:nil];
-	[self setLockScreenView:nil];
-	[super viewDidUnload];
+    [self setInfoLabel:nil];
+    [self setLockScreenView:nil];
+    [super viewDidUnload];
 }
 @end
