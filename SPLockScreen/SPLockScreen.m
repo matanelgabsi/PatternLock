59ec6cc0df61d2dc65631f94874c0ac1ae620eee
matanelgabsi <matan.elgabsi@gmail.com>
2016-01-16 20:47:06 +0200

    Fixed some bugs & added features
    Added option to select difference padding to X and Y, also now when bounds are changed, the view will refresh.

--- SPLockScreen/SPLockScreen.m
+++ SPLockScreen/SPLockScreen.m
@@ -25,11 +25,12 @@
 
 
 @interface SPLockScreen ()
-@property (nonatomic, strong) NormalCircle        *selectedCell;
 @property (nonatomic, strong) SPLockOverlay       *overLay;
-@property (nonatomic) NSInteger                   oldCellIndex, currentCellIndex;
+@property (nonatomic) NSInteger                   oldCellIndex;
 @property (nonatomic, strong) NSMutableDictionary *drawnLines;
 @property (nonatomic, strong) NSMutableArray      *finalLines, *cellsInOrder;
+@property (nonatomic, strong) NSMutableArray      *circles;
+@property (nonatomic, assign) BOOL                initWasDone;
 
 @property (nonatomic) BOOL allowClosedPattern;            // Set to YES to allow a closed pattern, a complex type pattern; NO by default
 
@@ -40,7 +41,8 @@
 @property (nonatomic, strong) UIColor *innerColor;
 @property (nonatomic, strong) UIColor *highlightColor;
 
-@property (nonatomic) CGFloat padding;
+@property (nonatomic) CGFloat paddingX;
+@property (nonatomic) CGFloat paddingY;
 @property (nonatomic) CGFloat radius;
 
 - (void)resetScreen;
@@ -60,32 +62,24 @@
 }
 
 - (void)willMoveToSuperview:(UIView *)newSuperview {
+    [self initialSetup];
     [self setDefaults];
     [self setupView];
 }
 
-- (void) setFrame: (CGRect) frame {
-    [super setFrame: frame];
+-(void)setBounds:(CGRect)newBounds {
+    [super setBounds:newBounds];
+    [self.overLay setFrame:self.bounds];
+    [self initialSetup];
     [self setDefaults];
     [self setupView];
 }
 
-- (void)setupView {
-    CGFloat gap = (self.frame.size.width - self.padding * 2 - self.radius * 6) / 2;
-
-    for (NSUInteger i          = 0; i < 9; i++) {
-        NormalCircle *circle = [[NormalCircle alloc] initWithRadius:self.radius];
-        circle.innerColor     = self.innerColor;
-        circle.outerColor     = self.outerColor;
-        circle.highlightColor = self.highlightColor;
-        NSUInteger column = i % 3;
-        NSUInteger row    = i / 3;
-        CGFloat    x      = self.padding + self.radius + (gap + 2 * self.radius) * column;
-        CGFloat    y      = self.padding + self.radius + (gap + 2 * self.radius) * row;
-        circle.center = CGPointMake(x, y);
-        circle.tag    = (row + kSeed) * kTagIdentifier + (column + kSeed);
-        [self addSubview:circle];
+- (void) initialSetup {
+    if (self.initWasDone) {
+        return;
     }
+    self.circles = [NSMutableArray new];
     self.drawnLines            = [[NSMutableDictionary alloc] init];
     self.finalLines            = [[NSMutableArray alloc] init];
     self.cellsInOrder          = [[NSMutableArray alloc] init];
@@ -94,13 +88,39 @@
     self.overLay.lineColor     = self.lineColor;
     self.overLay.lineGridColor = self.lineGridColor;
     [self.overLay setUserInteractionEnabled:NO];
+    
     [self addSubview:self.overLay];
     // set selected cell indexes to be invalid
-    self.currentCellIndex = -1;
-    self.oldCellIndex     = self.currentCellIndex;
-
-    [self setNeedsDisplay];
+    self.oldCellIndex     = -1;
+    
     [self addGestureRecognizer];
+    self.initWasDone = YES;
+}
+
+- (void)setupView {
+    for (UIView *circle in self.circles) {
+        [circle removeFromSuperview];
+    }
+    [self.circles removeAllObjects];
+    CGFloat gapX = (self.frame.size.width - self.paddingX * 2 - self.radius * 6) / 2;
+    CGFloat gapY = (self.frame.size.height - self.paddingY * 2 - self.radius * 6) / 2;
+
+    for (NSUInteger i          = 0; i < 9; i++) {
+        NormalCircle *circle = [[NormalCircle alloc] initWithRadius:self.radius];
+        circle.innerColor     = self.innerColor;
+        circle.outerColor     = self.outerColor;
+        circle.highlightColor = self.highlightColor;
+        NSUInteger column = i % 3;
+        NSUInteger row    = i / 3;
+        CGFloat    x      = self.paddingX + self.radius + (gapX + 2 * self.radius) * column;
+        CGFloat    y      = self.paddingY + self.radius + (gapY + 2 * self.radius) * row;
+        circle.center = CGPointMake(x, y);
+        circle.tag    = (row + kSeed) * kTagIdentifier + (column + kSeed);
+        [self addSubview:circle];
+        [self.circles addObject:circle];
+    }
+    
+    [self setNeedsDisplay];
 }
 
 - (void)setDefaults {
@@ -110,7 +130,8 @@
     self.innerColor     = kInnerColor;
     self.highlightColor = kHighlightColor;
     self.radius         = 35.0;
-    self.padding        = 20.0;
+    self.paddingX        = 20.0;
+    self.paddingY        = 20.0;
 
     if (self.delegate) {
         if ([self.delegate respondsToSelector:@selector(lineColor)]) {
@@ -131,8 +152,11 @@
         if ([self.delegate respondsToSelector:@selector(radius)]) {
             self.radius = self.delegate.radius;
         }
-        if ([self.delegate respondsToSelector:@selector(padding)]) {
-            self.padding = self.delegate.padding;
+        if ([self.delegate respondsToSelector:@selector(paddingX)]) {
+            self.paddingX = self.delegate.paddingX;
+        }
+        if ([self.delegate respondsToSelector:@selector(paddingY)]) {
+            self.paddingY = self.delegate.paddingY;
         }
     }
 
@@ -145,26 +169,7 @@
     for (UIView *view in self.subviews) {
         if ([view isKindOfClass:[NormalCircle class]]) {
             if (CGRectContainsPoint(view.frame, point)) {
-                NormalCircle *cell = (NormalCircle *)view;
-
-                if (!cell.selected) {
-                    [cell highlightCell];
-                    self.currentCellIndex = [self indexForCell:cell];
-                    self.selectedCell     = cell;
-                }
-
-                else if (cell.selected) {
-                    if ([self.delegate respondsToSelector:@selector(allowClosedPattern)]) {
-                        if (self.delegate.allowClosedPattern) {
-                            self.currentCellIndex = [self indexForCell:cell];
-                            self.selectedCell     = cell;
-                        }
-                    }
-                }
-
-                NSInteger row    = view.tag / kTagIdentifier - kSeed;
-                NSInteger column = view.tag % kTagIdentifier - kSeed;
-                return row * 3 + column;
+                return [self indexForCell: view];
             }
         }
     }
@@ -189,18 +194,22 @@
 }
 
 - (void)handlePanAtPoint:(CGPoint)point {
-    self.oldCellIndex = self.currentCellIndex;
     NSInteger cellPos = [self indexForPoint:point];
 
-    if (cellPos >= 0 && cellPos != self.oldCellIndex && [self.cellsInOrder indexOfObject:@(self.currentCellIndex)] == NSNotFound) {
-        [self.cellsInOrder addObject:@(self.currentCellIndex)];
+    if (cellPos >= 0 && cellPos != self.oldCellIndex && [self.cellsInOrder indexOfObject:@(cellPos)] == NSNotFound) {
+        NSLog(@"Added point %d", cellPos);
+        if (self.oldCellIndex != -1) {
+            SPLine *aLine = [[SPLine alloc] initWithFromPoint:[self cellAtIndex:self.oldCellIndex].center
+                                                  toPoint:[self cellAtIndex:cellPos].center
+                                          AndIsFullLength:YES];
+            [self.finalLines addObject:aLine];
+        }
+        
+        [self.cellsInOrder addObject:@(cellPos)];
+        self.oldCellIndex = cellPos;
+        [[self cellAtIndex:cellPos] highlightCell];
     }
-
-    if (cellPos < 0 && self.oldCellIndex < 0) {
-        return;
-    }
-
-    else if (cellPos < 0) {
+    if (self.oldCellIndex != -1) {
         SPLine *aLine = [[SPLine alloc] initWithFromPoint:[self cellAtIndex:self.oldCellIndex].center
                                                   toPoint:point
                                           AndIsFullLength:NO];
@@ -208,30 +217,6 @@
         [self.overLay.pointsToDraw addObjectsFromArray:self.finalLines];
         [self.overLay.pointsToDraw addObject:aLine];
         [self.overLay setNeedsDisplay];
-    }
-    else if (cellPos >= 0 && self.currentCellIndex == self.oldCellIndex) {
-        return;
-    }
-    else if (cellPos >= 0 && self.oldCellIndex == -1) {
-        return;
-    }
-    else if (cellPos >= 0 && self.oldCellIndex != self.currentCellIndex) {
-        // two situations: line already drawn, or not fully drawn yet
-        NSNumber *uniqueId = [self uniqueLineIdForLineJoiningPoint:self.oldCellIndex AndPoint:self.currentCellIndex];
-
-        if (!(self.drawnLines)[uniqueId]) {
-            SPLine *aLine = [[SPLine alloc] initWithFromPoint:[self cellAtIndex:self.oldCellIndex].center
-                                                      toPoint:self.selectedCell.center
-                                              AndIsFullLength:YES];
-            [self.finalLines addObject:aLine];
-            [self.overLay.pointsToDraw removeAllObjects];
-            [self.overLay.pointsToDraw addObjectsFromArray:self.finalLines];
-            [self.overLay setNeedsDisplay];
-            self.drawnLines[uniqueId] = @(YES);
-        }
-        else {
-            return;
-        }
     }
 }
 
@@ -245,13 +230,13 @@
 }
 
 - (NSNumber *)patternToUniqueId {
-    NSMutableString *numberString = [NSMutableString new];
+    NSUInteger finalNumber = 0;
 
     for (NSInteger i = 0; i < self.cellsInOrder.count; i++) {
-        NSNumber *thisNumber = self.cellsInOrder[(NSUInteger)i];
-        [numberString appendString:thisNumber.stringValue];
+        NSUInteger thisNumber = [self.cellsInOrder[(NSUInteger)i] unsignedLongValue] + 1;
+        finalNumber = finalNumber * 10 + thisNumber;
     }
-    return @(numberString.integerValue);
+    return @(finalNumber);
 }
 
 - (void)resetScreen {
@@ -266,8 +251,6 @@
     [self.overLay.pointsToDraw removeAllObjects];
     [self.overLay setNeedsDisplay];
     self.oldCellIndex     = -1;
-    self.currentCellIndex = -1;
-    self.selectedCell     = nil;
 }
 
 
@@ -297,9 +280,8 @@
     }
     else {
         NSInteger cellPos = [self indexForPoint:point];
-        self.oldCellIndex = self.currentCellIndex;
         if (cellPos >= 0) {
-            [self.cellsInOrder addObject:@(self.currentCellIndex + 1)];
+            [self.cellsInOrder addObject:@(cellPos)];
             [self performSelector:@selector(endPattern) withObject:nil afterDelay:0.3];
         }
     }
